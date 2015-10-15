db861e98f40259285c8c0fd1eb5231a52eadc8b8
Caleb Hoover <choover11@gmail.com>
2015-10-14 17:09:41 -0700

    changed to using formatters so code isn't duplicated

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 var GulpUtil = require('gulp-util');
 var Chalk = require('chalk');
 var Table = require('cli-table');
-var RequireSafe = require('requiresafe');
+var RequireSafe = require('../cli');
+var format = RequireSafe.formatters.default;
 
 var rsGulp = function (params, callback) {
 
@@ -19,51 +20,19 @@ var rsGulp = function (params, callback) {
 
   RequireSafe.check(payload, function (err, data) {
 
-    if (err) {
-      return callback(Chalk.yellow('(+) ') + err);
+    var stack = format(err, data);
+
+    if (err || data.length) {
+      if (params.stopOnError === false) {
+        GulpUtil.log(stack);
+        return callback();
+      }; 
     }
-
-    var width = 80;
-    if (process.stdout.isTTY) {
-      width = process.stdout.getWindowSize()[0] - 10;
-    }
-
-    var stack = '\n';
-
-    if (data.length === 0) {
-
-      stack = stack + Chalk.green('(+)') + ' No known vulnerabilities found';
-    } else {
-
-      data.map(function (finding) {
-
-        var table = new Table({
-          head: ['', finding.title],
-          colWidths: [15, width - 15]
-        });
-
-        table.push(['Name', finding.module]);
-        table.push(['Version', finding.version]);
-        table.push(['Path', finding.path]);
-        table.push(['More Info', finding.advisory]);
-
-        stack = stack + table.toString() + '\n';
-
-      });
-
-      stack = stack + Chalk.red('(+) ') + data.length + ' vulnerabilities found\n';
-    }
-
-    if (params.stopOnError === false) {
-      GulpUtil.log(stack);
-      return callback();
-    };
 
     return callback({
       stack: stack,
-      message: Chalk.red('(+) ') + data.length + ' vulnerabilities found\n'
+      message: stack.split('\n')[0]
     });
-
 
   });
 
