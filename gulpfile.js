2ce22a929645a50e1bcc2c858db41db5e729ddf0
Michael Garvin <gar+gh@danger.computer>
2016-03-30 12:02:42 -0700

    add gulpfile so we can test that this works in CI

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var Gulp = require('gulp');
+var GulpNSP = require('./index.js');
+
+Gulp.task('nsp', function (cb) {
+  GulpNSP({
+    shrinkwrap: __dirname + '/npm-shrinkwrap.json',
+    package: __dirname + '/package.json'
+  }, cb);
+});
